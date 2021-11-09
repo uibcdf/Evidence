c7748d5e89e54f86c351593d6b8d2931924cfca6
Diego Prada <prada.gracia@gmail.com>
2021-11-09 12:05:26 -0600

    Fixing script to update conda env files.

--- devtools/conda-envs/update_env_files.py
+++ devtools/conda-envs/update_env_files.py
@@ -1,68 +1,67 @@
 import yaml
 
-def flatten(arg):
+def heal(arg):
 
     output = []
-    if type(arg) in [list, tuple]:
 
-        for ii in arg:
-            if type(ii) in [list, tuple]:
-                output.extend(ii)
-            else:
+    for ii in arg:
+        if type(ii) is list:
+            for jj in ii:
+                if jj not in output:
+                    output.append(jj)
+        else:
+            if ii not in output:
                 output.append(ii)
 
     return output
 
-
 with open('../requirements.yaml') as fff:
     all_requirements = yaml.load(fff, Loader=yaml.FullLoader)
 
-
-
 # Production
 env_dict={}
-env_dict["channels"]=flatten(all_requirements["production"]["channels"])
-env_dict["dependencies"]=flatten(all_requirements["production"]["dependencies"])
+env_dict["channels"]=heal(all_requirements["production"]["channels"])
+env_dict["dependencies"]=heal(all_requirements["production"]["dependencies"])
 fff = open("production_env.yaml", "w")
 yaml.dump(env_dict, fff, sort_keys=True)
 fff.close()
 
 # Development
 env_dict={}
-env_dict["channels"]=flatten(all_requirements["development"]["channels"])
-env_dict["dependencies"]=flatten(all_requirements["development"]["dependencies"])
+env_dict["channels"]=heal(all_requirements["development"]["channels"])
+env_dict["dependencies"]=heal(all_requirements["development"]["dependencies"])
 fff = open("development_env.yaml", "w")
 yaml.dump(env_dict, fff, sort_keys=True)
 fff.close()
 
 # Test
 env_dict={}
-env_dict["channels"]=flatten(all_requirements["test"]["channels"])
-env_dict["dependencies"]=flatten(all_requirements["test"]["dependencies"])
+env_dict["channels"]=heal(all_requirements["test"]["channels"])
+env_dict["dependencies"]=heal(all_requirements["test"]["dependencies"])
 fff = open("test_env.yaml", "w")
 yaml.dump(env_dict, fff, sort_keys=True)
 fff.close()
 
 # Docs
 env_dict={}
-env_dict["channels"]=flatten(all_requirements["docs"]["channels"])
-env_dict["dependencies"]=flatten(all_requirements["docs"]["dependencies"])
+env_dict["channels"]=heal(all_requirements["docs"]["channels"])
+env_dict["dependencies"]=heal(all_requirements["docs"]["dependencies"])
 fff = open("docs_env.yaml", "w")
 yaml.dump(env_dict, fff, sort_keys=False)
 fff.close()
 
 # Setup
 env_dict={}
-env_dict["channels"]=flatten(all_requirements["setup"]["channels"])
-env_dict["dependencies"]=flatten(all_requirements["setup"]["dependencies"])
+env_dict["channels"]=heal(all_requirements["setup"]["channels"])
+env_dict["dependencies"]=heal(all_requirements["setup"]["dependencies"])
 fff = open("setup_env.yaml", "w")
 yaml.dump(env_dict, fff, sort_keys=False)
 fff.close()
 
 # Build
 env_dict={}
-env_dict["channels"]=flatten(all_requirements["conda-build"]["channels"])
-env_dict["dependencies"]=flatten(all_requirements["conda-build"]["dependencies"])
+env_dict["channels"]=heal(all_requirements["conda-build"]["channels"])
+env_dict["dependencies"]=heal(all_requirements["conda-build"]["dependencies"])
 fff = open("build_env.yaml", "w")
 yaml.dump(env_dict, fff, sort_keys=False)
 fff.close()
