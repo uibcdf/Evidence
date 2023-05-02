9466b22c2d0b2cc0b8e4d603aa2b159f9253111a
Diego Prada <prada.gracia@gmail.com>
2023-05-01 18:20:20 -0600

    Moving to Python 3.10 in process.

--- evidence/__init__.py
+++ evidence/__init__.py
@@ -2,13 +2,8 @@
 Evidence
 This must be a short description of the project
 """
-
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+# versioningit
+from ._version import __version__
 
 __documentation_web__ = 'https://www.uibcdf.org/evidence'
 __github_web__ = 'https://github.com/uibcdf/evidence'
